553a482e1ef7252b0fd8d966b9c020624717c85a
Kiren Paul <kirenpaul@gmail.com>
2022-05-23 14:54:25 +0700

    new app file added

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar, useColorScheme, View} from 'react-native';
+import {Text} from 'react-native-ui-lib';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+
+      <View
+        style={{
+          backgroundColor: isDarkMode ? Colors.black : Colors.white,
+        }}>
+        <Text> Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
