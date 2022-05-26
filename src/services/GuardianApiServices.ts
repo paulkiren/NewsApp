291769f863a943bdd2d7cfb0333a57885de0cb04
Kiren Paul <kirenpaul@gmail.com>
2022-05-26 13:48:16 +0700

    adding guardian sample api

---
+++ src/services/GuardianApiServices.ts
@@ -0,0 +1,14 @@
+const GuardianAPiService = {
+  getNewsFromApi: () => {
+    return fetch("https://content.guardianapis.com/search?q=debate%20AND%20NOT%20immigration&tag=politics/politics&from-date=2014-01-01&api-key=test")
+      .then(response => response.json())
+      .then(json => {
+        return json.movies;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  },
+};
+
+export default GuardianAPiService;
