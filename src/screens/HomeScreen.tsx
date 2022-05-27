d99d223f96ba1f70d594ada0ccbbabc5335d31d5
Kiren Paul <kirenpaul@gmail.com>
2022-05-27 11:33:53 +0700

    adding flatlist to load news  from test api

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,94 @@
-import React from 'react';
-import {View} from 'react-native-ui-lib';
-import BottomBar from '../components/BottomBar';
-
-const HomeScreen = () => {
+import React, {useState, useEffect} from 'react';
+import {View, FlatList, Text, StyleSheet, StatusBar} from 'react-native';
+import GuardianApiServices from '../services/GuardianApiServices';
+// const Item = (title: string) => (
+//   <View style={styles.item}>
+//     <Text style={styles.title}>{title}</Text>
+//   </View>
+// );
+const Item = ({item}: {item: INewsData}) => {
+  console.log("MY Item ", item);
   return (
-    <View>
-      <View> Hello </View>
-      <BottomBar></BottomBar>
+    <View style={styles.item}>
+      <Text style={styles.title}>{item.webTitle}</Text>
+    </View>
+  );
+};
+export interface INewsData {
+  id: string;
+  type: string;
+  sectionId: string;
+  sectionName: string;
+  webPublicationDate: Date;
+  webTitle: string;
+  webUrl: string;
+  apiUrl: string;
+  isHosted: boolean;
+  pillarId: string;
+  pillarName: string;
+}
+
+export interface Response {
+  status: string;
+  userTier: string;
+  total: number;
+  startIndex: number;
+  pageSize: number;
+  currentPage: number;
+  pages: number;
+  orderBy: string;
+  results: INewsData[];
+}
+
+const renderItem = ({item}: {item: INewsData}) => <Item item={item} />;
+const HomeScreen = () => {
+  const [isLoadingData, setIsLoadingData] = useState(false);
+  const [newsData, setnewsData] = useState([]);
+
+  useEffect(() => {
+    setIsLoadingData(true);
+    GuardianApiServices.getNewsFromApi()
+      .then(response => {
+        const myresponse = response.response;
+        console.log('My response ', response.response, myresponse.results);
+
+        if (myresponse.status == 'ok') setnewsData(myresponse?.results);
+      })
+      .catch(e => {
+        console.log('Api error', e);
+      })
+      .finally(() => {
+        setIsLoadingData(false);
+      });
+  }, []);
+  return (
+    <View   style={{flex: 1, backgroundColor: 'violet'}}>
+      <Text> {'Hello'} </Text>
+      <FlatList
+        data={newsData}
+        renderItem={renderItem}
+        style={{flex: 1, backgroundColor: 'yellow'}}
+        keyExtractor={item => item.id}
+        refreshing={isLoadingData}
+      />
     </View>
   );
 };
 
-export default  HomeScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
+
+export default HomeScreen;
--- src/services/GuardianApiServices.ts
+++ src/services/GuardianApiServices.ts
@@ -3,7 +3,7 @@ const GuardianAPiService = {
     return fetch("https://content.guardianapis.com/search?q=debate%20AND%20NOT%20immigration&tag=politics/politics&from-date=2014-01-01&api-key=test")
       .then(response => response.json())
       .then(json => {
-        return json.movies;
+        return json;
       })
       .catch(error => {
         console.error(error);
